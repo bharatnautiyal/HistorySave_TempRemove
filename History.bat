55bfa82f57581716f1172b6c378f125f08562b95
Bharat Nautiyal <bharatnautiyal007@gmail.com>
2022-10-03 23:57:38 +0530

    adding commt in history file

--- History.bat
+++ History.bat
@@ -1,6 +1,7 @@
+#TEST
 @echo off
 :top
 for /f "delims=: tokens=2" %%i in ('ipconfig /displaydns^|find "Record Name"') do (find "%%i" /i history.txt >nul 2>&1|| echo %%i >>history.txt)
 timeout /nobreak 5 >nul 2>&1
-sort history.txt /o history.txt
-goto top
+sort history.txt /o history.txT
+goto TOP
