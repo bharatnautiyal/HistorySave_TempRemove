4d223e9869d1ec7340b676bb40bd88440d2f90d4
Bharat Nautiyal <bharatnautiyal007@gmail.com>
2022-10-03 23:59:43 +0530

    small changes

--- History.bat
+++ History.bat
@@ -1,7 +1,6 @@
-#TEST
 @echo off
 :top
 for /f "delims=: tokens=2" %%i in ('ipconfig /displaydns^|find "Record Name"') do (find "%%i" /i history.txt >nul 2>&1|| echo %%i >>history.txt)
 timeout /nobreak 5 >nul 2>&1
-sort history.txt /o history.txT
-goto TOP
+sort history.txt /o history.txt
+goto top
